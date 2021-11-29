299f168ed543014ef69074b65ba5f16d872695c3
lucasdemicco <lucasmicco@hotmail.com>
2021-11-29 20:05:17 -0300

    "Adicionando Mapping ao projeto com AutoMapper"

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +17,10 @@ var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options => options
     .UseMySql(connection,
         new MySqlServerVersion(new Version(8,0,5))));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
