dbee3f30a4a3add911e6ed1e8f2b1b4a61218dfb
lucasdemicco <lucasmicco@hotmail.com>
2021-11-30 17:10:10 -0300

    OnModelCreating de Context criado vazio

--- GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -10,5 +10,10 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
         
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
     }
 }
