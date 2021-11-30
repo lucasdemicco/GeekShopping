f26134ec1668428cb84ccf2c022d7523221e19bc
lucasdemicco <lucasmicco@hotmail.com>
2021-11-30 17:34:48 -0300

    "Implementando HttpClientExtensions" - FRONTEND

---
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"Algo aconteceu de errado com a API: {response.ReasonPhrase}");
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString, 
+                   new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
