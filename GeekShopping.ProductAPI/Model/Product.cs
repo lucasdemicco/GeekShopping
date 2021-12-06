61b31c21f1a2bf047369046fec3602d745ffd520
lucasdemicco <lucasmicco@hotmail.com>
2021-12-06 10:02:25 -0300

    Aplicando validations na Classe de modelo Produto

--- GeekShopping.ProductAPI/Model/Product.cs
+++ GeekShopping.ProductAPI/Model/Product.cs
@@ -9,7 +9,7 @@ namespace GeekShopping.ProductAPI.Model
     {
         [Column("Name")]
         [Required]
-        [StringLength(150)]
+        [StringLength(150, ErrorMessage = "Nome pode ter no máximo 150 caracteres")]
         public string Name { get; set; }
 
         [Column("Price")]
@@ -18,16 +18,38 @@ namespace GeekShopping.ProductAPI.Model
         public decimal Price { get; set; }   
         
         [Column("Description")]
-        [StringLength(500)]
+        [StringLength(500, ErrorMessage = "Descrição de no máximo 500 caracteres")]
         public string Description { get; set; }
 
         [Column("Category_Name")]
         [Required]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "Nome da categoria pode conter no máximo 50 caracteres")]
         public string CategoryName { get; set;}
 
         [Column("Image_Url")]
-        [StringLength(300)]
+        [StringLength(300, ErrorMessage = "String URL de no máximo 300 caracteres")]
         public string ImageUrl { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var primeiraLetra = this.Name[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new
+                    ValidationResult("A primeira letra deve ser maiúscula",
+                new[]
+                { nameof(this.Name) }
+                );
+            }
+
+            if(this.Price < 0)
+            {
+                yield return new
+                    ValidationResult("O preço deve ser maior ou igual a zero",
+                    new[]
+                    { nameof(this.Price) }
+                    );
+            }
+        }
     }
 }
