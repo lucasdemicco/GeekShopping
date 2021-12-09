1212004b090a7f3655ce996dc8d39020b8616ccd
lucasdemicco <lucasmicco@hotmail.com>
2021-12-09 14:30:40 -0300

    Adicionando authorize na Api

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -65,6 +66,41 @@ builder.Services.AddApiVersioning(opt =>
     opt.AssumeDefaultVersionWhenUnspecified = true;
     opt.DefaultApiVersion = new ApiVersion(1, 0);
     opt.ReportApiVersions = true;
+});
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    var security = new Dictionary<string, IEnumerable<string>>
+    {
+        {"Bearer", new string[] { }}
+    };
+
+    opt.AddSecurityDefinition(
+        "Bearer",
+        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme."
+        });
+
+        opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+       {
+          {
+             new OpenApiSecurityScheme
+             {
+                Reference = new OpenApiReference
+                {
+                   Type = ReferenceType.SecurityScheme,
+                   Id = "Bearer"
+                }
+             },
+             new string[] {}
+          }
+    });
 });
  
 var app = builder.Build();
